47cb32d98e2a5f9f438193dc192c3ec0dd495376
Daehder <erikm95@me.com>
2016-04-22 16:56:14 -0700

    I hope this is where we want to be

--- Project2/Project2/main.c
+++ Project2/Project2/main.c
@@ -1,10 +1,10 @@
 /*
-Students: Mytch Johnson and Erik Miller
-Class: CPE 329-07 w/ Dr. Oliver
-Project 2: Create a function generator
-Date: 4/20/2016
-Revision: 1
-*/
+ Students: Mytch Johnson and Erik Miller
+ Class: CPE 329-07 w/ Dr. Oliver
+ Project 2: Create a function generator
+ Date: 4/20/2016
+ Revision: 1
+ */
 
 // I hope this is where we want to be
 
@@ -12,142 +12,48 @@ int num_samples =  50;			// sets global number of samples
 int overflow_100Hz = 200;		// set overflow value for 100Hz
 int overflow_100hz = 40;		// set overflow value for 500Hz
 
-#include <avr/io.h>
-#include <util/delay.h>
-#include <avr/interrupt.h>
 #include "WaveGen.h"
-
-// initialization helper functions
-void Initialize_SPI_Master();
-void Transmit_SPI_Master(int Data);
-void GPIO_Initialization();
-void initTimer0();
-void genorate_LUTs();
-
-// general helper functions
-void delay_us(uint16_t delay);
-void delay_ms(uint16_t delay);
-uint8_t check_buttons();
-
-// Program 2 specific functions
-uint16_t volts_to_bits(double voltage);
+#include "arduinoUtil.h"
 
 // Global Variables
 uint8_t LUT_address = 0;
 
 int main(void)
-{	
-	// initialize GPIO, interrupts, and timers
-	GPIO_Initialization();
-	
-	// fill square, sawtooth, triangle, and sine wave LUTs
-	genorate_LUTs();
-
-	while(1){
-		if(check_buttons())
-			PORTB |= (1<<LED12);
-		else 
-			PORTB &= ~(1<<LED12);
-	}
-	
-    return 0;
-}  
-
-// sets up SPI system between ATmega328P and slave device(s)
-void Initialize_SPI_Master(){     
-    SPCR = (0<<SPIE) |		//No interrupts
-    (1<<SPE) |				//SPI enabled
-    (1<<MSTR) ;             //master
-    SPSR = 0x00;			//clear flags
-    PORTB = 1 << SS;		// make sure SS is high
-}
-
-// Initializing timer 0 (change OCR0A for freq change!!!!!)
-void initTimer0(){
-	TCCR0A = 0x02;			// timer CTC mode
-	OCR0A = OVERFLOW_100Hz;	// sets counter overflow to 250
-	TCCR0B = 0x02;			// timer clk = system clk / 8 (2MHz)
-	TIFR0 = 0x02;			// Interrupt occurs at OCRF0A overflow 
-	TIMSK0 = 0x02;			// OCRF0A overflow interrupt enabled
-}
-
-
-// initializes GPIO I/O, SPI interface, and interrupts (working)
-void GPIO_Initialization(){
-	DDRB |= (1<<MOSI) |						// make MOSI, SCK and SS outputs
-			(1<<SCK)  | 
-			(1<<SS);
-	DDRD |= (1<<LED12);						// led at PB4 is output
-	DDRD &= ~(1<<BTN0) |					// buttons 0 and 1 are inputs
-			~(1<<BTN1);
-	PORTD |= (1<<BTN0) | (1<<BTN1);			// set internal pull-ups on btns
-	Initialize_SPI_Master();				// initialize SPI to DAC
-	initTimer0();							// initialize timer0
+{
+   // initialize GPIO, interrupts, and timers
+   GPIO_Initialization();
+   
+   // fill square, sawtooth, triangle, and sine wave LUTs
+   genorate_LUTs();
+   
+   while (1){
+      if(check_buttons())
+         PORTB |= (1<<LED12);
+      else
+         PORTB &= ~(1<<LED12);
+   }
+   
+   return 0;
 }
 
 // fills all the lookup tables for every wave
 void genorate_LUTs(){
-	make_square_LUT();		// fill square wave LUT
-	make_sawtooth_LUT();	// fill sawtooth wave LUT
-	make_triangle_LUT();	// fill triangle wave LUT
-	make_sin_LUT();			// fill sine wave LUT
-	sei();
-}
-
-// returns a true bool if a buttons is pressed
-uint8_t check_buttons(){
-	if(!(PINB & (1<<BTN0)))			// return 1 if button at pin0 is pressed
-		return 1;
-	else if(!(PINB & (1<<BTN1)))	// return 2 if button at pin1 is pressed
-		return 2;
-	else
-		return 0;					// return 0 if no buttons are pressed
-}
-
-// sends data to DAC over SPI data port
-void Transmit_SPI_Master(int Data) { 
-PORTB &= ~(1 << SS);					//Assert slave select (active low)
-SPDR = ((Data >> 8) & 0xF) | 0x70;		//Attach configuration Bits onto MSB 
-while (!(SPSR & (1<<SPIF))); 
-SPDR = 0xFF & Data; 
-while (!(SPSR & (1<<SPIF))); 
-PORTB |= 1 << SS;						//Turn off slave select 
-}
-
-// allows delays with variable input for us range
-void delay_us(uint16_t delay){
-	while(delay){
-		_delay_us(1);
-		delay--;
-	}
-}
-
-// allows delays with variable input for ms range
-void delay_ms(uint16_t delay){
-	while(delay){
-		_delay_ms(1);
-		delay--;
-	}
-}
-
-// converts input voltage between 0-5V to 12bit number for DAC
-uint16_t volts_to_bits(double voltage){
-	double bits = ((voltage/5.0)*4095);
-	if(bits > 4095)						// if given above 5V return 5V
-		return 4095;
-	else
-		return bits;					// return 12bit equivalent for DAC
+   make_square_LUT();		// fill square wave LUT
+   make_sawtooth_LUT();	// fill sawtooth wave LUT
+   make_triangle_LUT();	// fill triangle wave LUT
+   make_sin_LUT();			// fill sine wave LUT
+   sei();
 }
 
 ///////////////////////////////////ISR/////////////////////////////////////////
 
 
-// ISR to increment through wave function LUTs and set frequency 
+// ISR to increment through wave function LUTs and set frequency
 ISR(TIMER0_COMPA_vect){
-	Transmit_SPI_Master(SinWave[LUT_address]);
-	LUT_address++;
-	
-	if(LUT_address>=NUM_SAMPLES)
-		LUT_address = 0;
+   Transmit_SPI_Master(SawWave[LUT_address]);
+   LUT_address++;
+   
+   if (LUT_address>=NUM_SAMPLES)
+      LUT_address = 0;
 }
 
